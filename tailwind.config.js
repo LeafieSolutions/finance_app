bfe1567657a4699f23b0d350ec8306b6c2c82f92
kimaru355 <emmanuelkimaru355@gmail.com>
2024-01-24 09:45:40 +0300

    tailwind check assets folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.{html,js}'],
+  content: [
+    './templates/**/*.{html,js}',
+    './assets/**/*.{html,js}'
+  ],
   theme: {
     extend: {},
   },
